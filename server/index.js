d7d1723d8cc6f7436cef3e02cfb8e07b2c4dc3ad
Tomer Becker <tbookra@gmail.com>
2022-09-12 11:38:39 +0300

    server side ready

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.get('/', (req,res) => {
 })
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
+app.use('/api/posts', postRoute)
 
 app.listen(8800, () => {
   console.log("server is running on 8800");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,38 @@
 const router = require("express").Router();
-const User = require('../models/Users')
+const User = require("../models/Users");
+const bcrypt = require("bcrypt");
 
 // Register
-router.post('/register', async (req,res)=>{
-    const user = await new User({
+router.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+    const newUser = await new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(`user was not created - ${error}`);
+  }
+});
 
-    })
-    user.save()
+router.post("/login", async (req,res) =>{
+    const {email, password} = req.body
+    try {
+        const user = await User.findOne({email})
+        !user && res.status(404).json("user not exists")
+        
+        const validPassword = await bcrypt.compare(password, user.password)
+        !validPassword && res.status(400).json("wrong user or password")
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
-
-
 
 module.exports = router;
